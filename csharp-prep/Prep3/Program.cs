26cbbcbfd9ca98e9bf3cd707484d1c0c6ed1a84f
Ricky Vang <vangcheemeng@yahoo.com>
2024-04-27 17:58:37 -0600

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        // Console.Write("What is the magic number? ");
+        // string num = Console.ReadLine();
+        // int magicNum = int.Parse(num);
+
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+
+
+
+
+        // Console.Write("What is your guess? ");
+        // string guess = Console.ReadLine();
+        // int finalGuess = int.Parse(guess);
+
+        // if (finalGuess > magicNum)
+        // {
+        //     Console.WriteLine("Lower");
+        // }
+        // else if (finalGuess < magicNum)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("You guessed it!");
+        // }
+
+
+        //BELOW: USING WHILE LOOP
+
+
+
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
