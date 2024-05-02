678cb45fa7ef45949dc933f3587853f39c986dbf
Ricky Vang <vangcheemeng@yahoo.com>
2024-05-01 20:13:04 -0600

    prep materials

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,7 @@
+public class Job {
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear ;
+    public int _endYear;
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+
+        Console.WriteLine(job2._company);
     }
 }
